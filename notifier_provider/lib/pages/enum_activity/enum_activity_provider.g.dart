fd9ca95e95f34a9764cf19a56586a882420afb3a
devseunghan <gankyou@naver.com>
2024-06-01 18:28:58 +0900

    notifier_provider 수강완료

---
+++ notifier_provider/lib/pages/enum_activity/enum_activity_provider.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'enum_activity_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$myCounterHash() => r'08aa79aa72de3999f55247db1de2cabfa13c9357';
+
+/// See also [MyCounter].
+@ProviderFor(MyCounter)
+final myCounterProvider = AutoDisposeNotifierProvider<MyCounter, int>.internal(
+  MyCounter.new,
+  name: r'myCounterProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$myCounterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MyCounter = AutoDisposeNotifier<int>;
+String _$enumActivityHash() => r'e9c7f4b5d0a34bd79ee3369fe613d0453d1926b4';
+
+/// See also [EnumActivity].
+@ProviderFor(EnumActivity)
+final enumActivityProvider =
+    AutoDisposeNotifierProvider<EnumActivity, EnumActivityState>.internal(
+  EnumActivity.new,
+  name: r'enumActivityProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$enumActivityHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$EnumActivity = AutoDisposeNotifier<EnumActivityState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
